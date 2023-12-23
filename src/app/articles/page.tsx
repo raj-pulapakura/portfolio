02fe57aad67599e69b8354739d480f1784c49b3a
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-12-23 14:05:48 +1100

    added medium and github links to articles and projects pages

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,13 +1,16 @@
 import ArticleItemLarge from "@/components/articles/ArticleItemLarge";
 import articles from "@/data/articles";
+import Link from "next/link";
 
 export default function ArticlesPage() {
   return (
     <main>
       <h1 className="text-4xl font-black mb-10 text-center">
-        <span className="underline underline-offset-4 decoration-light dark:decoration-dark">
-          Follow me on Medium
-        </span>
+        <Link href="https://medium.com/@raj.pulapakura">
+          <span className="underline underline-offset-4 decoration-light dark:decoration-dark">
+            Follow me on Medium
+          </span>
+        </Link>
         , where I write{" "}
         <span className="text-light dark:text-dark"> technical articles</span>,
         sharing my thoughts on software development and AI.
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,8 @@
 import ProjectItemLarge from "@/components/projects/ProjectItemLarge";
 import projects from "@/data/projects";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function ProjectsPage() {
   return (
@@ -8,6 +11,14 @@ export default function ProjectsPage() {
         I express my love for software development through the{" "}
         <span className="text-dark"> projects</span> I build.
       </h1>
+      <h2 className="text-lg font-bold mb-10 text-center">
+        <Link href="https://github.com/raj-pulapakura">
+          <span className="underline underline-offset-4 decoration-light dark:decoration-dark">
+            Check out my GitHub <FontAwesomeIcon icon={faGithub} />
+          </span>
+        </Link>{" "}
+        to see all my awesome projects.
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         {projects.map((project) => (
           <ProjectItemLarge project={project} key={project.title} />
