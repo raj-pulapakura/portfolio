5a5dd064e588a34ab5caa111f49ffb24b5f87a4a
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-12-30 22:54:46 +1100

    added date to small article component

--- src/components/articles/ArticleItemSmall.tsx
+++ src/components/articles/ArticleItemSmall.tsx
@@ -13,9 +13,10 @@ export default function ArticleItemsSmall({
       className={`flex flex-row shadow-boxlight dark:shadow-boxdark rounded-lg px-10 py-10 ${className}`}
     >
       <Link href={article.articleURL}>
-        <h1 className="text-2xl text-light dark:text-dark mb-5">
+        <h1 className="text-2xl text-light dark:text-dark mb-2">
           {article.title}
         </h1>
+        <p className="text-gray-400 mb-5">{article.datePublished}</p>
         <p>{article.subtitle}</p>
       </Link>
     </section>
