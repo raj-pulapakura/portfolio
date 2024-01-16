279772014913be86a69f3f866b57e86a686a25bc
Raj Pulapakura <raj.pulapakura@gmail.com>
2024-01-16 11:19:17 +1100

    updated summary

--- src/components/home/Summary.tsx
+++ src/components/home/Summary.tsx
@@ -7,10 +7,11 @@ export default function Summary() {
   return (
     <AnimationOnScroll animateOnce={true} animateIn="animate__bounceIn">
       <p className="text-center lg:text-left lg:shadow-boxlight lg:dark:shadow-boxdark lg:p-8 lg:rounded-2xl">
-        Hi, I'm Raj. I'm a software developer with expertise in full stack
-        web-development, cross-platform app development, and machine learning. I
-        love building beautiful and elegant interfaces, and combining them with
-        the power and functionality of machine learning!
+        Hi, I'm Raj. I'm a Machine Learning Engineer, Certified TensorFlow
+        Developer, and Full-Stack Engineer. I love ML and AI because of its
+        ability to solve human problems, eliminate inefficiencies, and reduce
+        human suffering. I specialize in building and deploying ML-powered
+        applications.
       </p>
     </AnimationOnScroll>
   );
